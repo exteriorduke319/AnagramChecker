c3a3fefc173701bb1b637e51a0d5fe6f3acc2faa
exteriorduke319 <yarnevictanner@gmail.com>
2023-11-09 09:32:00 -0500

    Init

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,44 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+
+        String a;
+        String b;
+        boolean anagramCheck = false;
+
+        System.out.println("Enter a word: ");
+        a = scan.nextLine();
+        a = a.toLowerCase();
+        System.out.println();
+        System.out.println("Enter another word, must be the same length: ");
+        b = scan.nextLine();
+        b = b.toLowerCase();
+
+
+        if (a.length() == b.length()) {
+            for(int i = 0; i < a.length(); i++) {
+                if(b.contains(a.substring(i, i+1))) {
+                    anagramCheck = true;
+                } else {
+                    anagramCheck = false;
+                    break;
+                }
+            }
+
+            if(anagramCheck) {
+                System.out.println();
+                System.out.println(a + " and " + b + " are anagrams.");
+            } else {
+                System.out.println();
+                System.out.println(a + " and " + b + " aren't anagrams.");
+            }
+
+        } else {
+            System.out.println();
+            System.out.println("Error: the words must be the same length");
+        }
+
     }
 }
